526c4a050f7de04fa9e584663d3b4cea8afe6941
Laptop <Jordaanh11@gmail.com>
2024-10-23 16:23:31 +0100

    WIP

--- PatientRecordSystem/PatientRecordSystem/obj/Debug/net8.0-windows/views/LoginView.g.i.cs
+++ PatientRecordSystem/PatientRecordSystem/obj/Debug/net8.0-windows/views/LoginView.g.i.cs
@@ -85,7 +85,7 @@ namespace PatientRecordSystem.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/PatientRecordSystem;component/views/loginview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/PatientRecordSystem;V1.0.0.0;component/views/loginview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\views\LoginView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
